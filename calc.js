0b57bab029b3254586abd44f2973ce85b2cbdbbe
Mehdi Safari <sa.mehdisafari@gmail.com>
2021-12-14 13:48:58 -0800

    remove unnecessary file

--- calc.js
+++ calc.js
@@ -13,6 +13,10 @@ reader.question("What would you like to calculate?", function(input){
     num1 = Number(tokens[1]);
     num2 = Number(tokens[2]);
 
+    console.log('mathSymbol', mathSymbol);
+    console.log('num1', num1);
+    console.log('num2', num2);
+
     if (mathSymbol === "+"){
           console.log(num1 + num2);
     }
